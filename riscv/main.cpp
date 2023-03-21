dd204c340cfcbff70aebef86b4f4eb2860184ea4
SeMinLim <seminl1@ics.uci.edu>
2023-03-21 04:55:55 -0700

    started to modified c code for riscv

--- riscv/main.cpp
+++ riscv/main.cpp
@@ -1,10 +1,17 @@
 #include <time.h>
 #include <sys/time.h>
+#include <sys/resource.h>
 
 #include "solver.h"
 
 
 // Elapsed time checker
+static inline double timeCheckerCPU(void) {
+        struct rusage ru;
+        getrusage(RUSAGE_SELF, &ru);
+        return (double)ru.ru_utime.tv_sec + (double)ru.ru_utime.tv_usec / 1000000;
+}
+
 static inline double timeCheckerREAL() {
         struct timeval tv;
         gettimeofday(&tv, NULL);
@@ -15,18 +22,23 @@ static inline double timeCheckerREAL() {
 int main( int argc, char **argv ) {
         Solver S;
 
+        double parseTimeInitCPU = timeCheckerCPU();
         double parseTimeInitREAL = timeCheckerREAL();
 
         int res = S.parse(argv[1]);
 
+        double parseTimeFinishCPU = timeCheckerCPU();
         double parseTimeFinishREAL = timeCheckerREAL();
 
+        double timeParseCPU = parseTimeFinishCPU - parseTimeInitCPU;
         double timeParseREAL = parseTimeFinishREAL - parseTimeInitREAL;
 
-        printf( "Parsing Time: %.2f\n", timeParseREAL );
+        printf( "Parsing Time (CPU): %.2f\n", timeParseCPU );
+        printf( "Parsing Time (REAL): %.2f\n", timeParseREAL );
 
         if ( res == 20 ) printf("s UNSATISFIABLE\n");
         else {
+                double processTimeInitCPU = timeCheckerCPU();
                 double processTimeInitREAL = timeCheckerREAL();
 
                 res = S.solve();
@@ -36,11 +48,14 @@ int main( int argc, char **argv ) {
                 }
                 else if ( res == 20 ) printf("s UNSATISFIABLE\n");
 
+                double processTimeFinishCPU = timeCheckerCPU();
                 double processTimeFinishREAL = timeCheckerREAL();
 
+                double timeProcessCPU = processTimeFinishCPU - processTimeInitCPU;
                 double timeProcessREAL = processTimeFinishREAL - processTimeInitREAL;
 
-                printf( "Elapsed Time: %.2f\n", timeProcessREAL );
+                printf( "Elapsed Time (CPU): %.2f\n", timeProcessCPU );
+                printf( "Elapsed Time (REAL): %.2f\n", timeProcessREAL );
 		printf( "Conflicts: %d\n", S.conflicts );
 		printf( "Decisions: %d\n", S.decides );
 		printf( "Propagations: %d\n", S.propagations );

005ba2d927064b44c57524a65197cd5a196d4137
SeMinLim <seminl1@ics.uci.edu>
2023-03-24 14:16:26 -0700

    added required files to make binary for bare-metal

---
+++ riscv/entry.s
@@ -0,0 +1,6 @@
+.text
+start:
+	li sp, 0x2000
+	jal main
+	.word 0
+
---
+++ riscv/strip.sh
@@ -0,0 +1,3 @@
+rm -f binary.bin
+objdump -h binary.elf
+objdump -h binary.elf | grep " \." | grep -v "comment" |  awk '{print "dd if=binary.elf bs=1 count=$[0x" $3 "] skip=$[0x" $6 "] seek=$[0x" $4 "] of=binary.bin";}' | bash

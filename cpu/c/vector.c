93b427ac6e23436f43910b9376bf41f62d805b66
SeMinLim <seminl1@ics.uci.edu>
2023-04-01 18:35:03 -0700

    added vector implementation in C code

--- cpu/c/vector.c
+++ cpu/c/vector.c
@@ -1,14 +1,14 @@
-void vector_init( vector *v ) {
+void vector_functions::init( vector *v ) {
 	v->capacity = VECTOR_INIT_CAPACITY;
 	v->total = 0;
 	v->items = malloc(sizeof(void*)*v->capacity);
 }
 
-int vector_size( vector *v ) {
+int vector_functions::size( vector *v ) {
 	return v->total;
 }
 
-static void vector_resize( vector *v, int capacity ) {
+static void vector_functions::recapacity( vector *v, int capacity ) {
 	void **items = realloc(v->items, sizeof(void*)*capacity);
 	if ( items ) {
 		v->items = items;
@@ -16,21 +16,31 @@ static void vector_resize( vector *v, int capacity ) {
 	}
 }
 
-void vector_pushback( vector *v, void *item ) {
-	if ( v->capacity == v->total ) vector_resize(v, v->capacity * 2);
+void vector_functions::resize( vector *v, int size, int value ) {
+	if ( size == v->total ) return;
+	else if ( size > v->total ) while ( (size - v->total) != 0 ) pushback(v, value);
+	else while ( (v->total - size) != 0 ) popback(v);
+}
+		
+void vector_functions::pushback( vector *v, void *item ) {
+	if ( v->capacity == v->total ) recapacity(v, v->capacity*2);
 	v->items[v->total++] = item;
 }
 
-void vector_set( vector *v, int index, void *item ) {
+void vector_functions::set( vector *v, int index, void *item ) {
 	if ( index >= 0 && index < v->total ) v->items[index] = item;
 }
 
-void *vector_get( vector *v, int index ) {
+void vector_functions::*get( vector *v, int index ) {
 	if ( index >= 0 && index < v->total ) return v->items[index];
 	return NULL;
 }
 
-void vector_delete( vector *v, int index ) {
+void vector_functions::*back( vector *v ) {
+	return v->items[v->total];
+}
+
+void vector_functions::delete( vector *v, int index ) {
 	if ( index < 0 || index >= v->total ) return;
 	
 	v->items[index] = NULL;
@@ -42,10 +52,17 @@ void vector_delete( vector *v, int index ) {
 	
 	v->total--;
 	
-	if ( v->total > 0 && v->total == v->capacity / 4 ) vector_resize(v, v->capacity / 2);
+	if ( v->total > 0 && v->total == v->capacity / 4 ) recapacity(v, v->capacity / 2);
 }
 
-void vector_clear( vector *v ) {
+void vector_functions::popback( vector *v ) {
+	v->items[v->total] = NULL;
+	v->total--;
+
+	if ( v->total > 0 && v->total == v->capacity / 4 ) recapacity(v, v->capacity / 2);
+}
+
+void vector_functions::clear( vector *v ) {
 	free(v->items);
 }
 
--- cpu/c/vector.h
+++ cpu/c/vector.h
@@ -1,29 +1,42 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
 
 #define VECTOR_INIT_CAPACITY 4
 
-#define VECTOR_INIT(vec) vector vec; vector_init(&vec)
-#define VECTOR_SIZE(vec) vector_size(&vec)
-#define VECTOR_PUSHBACK(vec, item) vector_pushback(&vec, (void *) item)
-#define VECTOR_SET(vec, id, item) vector_set(&vec, id, (void *) item)
-#define VECTOR_GET(vec, type, id) (type) vector_get(&vec, id)
-#define VECTOR_DELETE(vec, id) vector_delete(&vec, id)
-#define VECTOR_CLEAR(vec) vector_clear(&vec)
+#define VECTOR_INIT(vec) vector vec; vector_functions.init(&vec)
+#define VECTOR_SIZE(vec) vector_functions.size(&vec)
+#define VECTOR_RESIZE(vec, size, value) vector_functions.resize(&vec, size, value)
+#define VECTOR_PUSHBACK(vec, item) vector_functions.pushback(&vec, (void*) item)
+#define VECTOR_SET(vec, id, item) vector_functions.set(&vec, id, (void*) item)
+#define VECTOR_GET(vec, type, id) (type*) vector_functions.get(&vec, id)
+#define VECTOR_BACK(vec, type) (type*) vector_functions.back(&vec)
+#define VECTOR_DELETE(vec, id) vector_functions.delete(&vec, id)
+#define VECTOR_POPBACK(vec) vector_functions.popback(&vec)
+#define VECTOR_CLEAR(vec) vector_functions.clear(&vec)
 
 
-struct vector {
+typedef struct vector {
 	void **items;
 	int capacity;
 	int total;
-};
+} vector;
 
-void vector_init( vector *v );
-int vector_size( vector *v );
-static void vector_resize( vector *v, int capacity );
-void vector_pushback( vector *v, void *item );
-void vector_set( vector *v, int index, void *item );
-void *vector_get( vector *v, int index );
-void vector_delete( vector *v, int index );
-void vector_clear( vector *v );
+typedef struct vector_functions {
+	void init( vector *v );
+	int size( vector *v );
+	static void recapacity( vector *v, int capacity );
+	void resize( vector *v, int size, int value );
+	void pushback( vector *v, void *item );
+	void set( vector *v, int index, void *item );
+	void *get( vector *v, int index );
+	void *back( vector *v );
+	void delete( vector *v, int index );
+	void popback( vector *v );
+	void clear( vector *v );
+} vector_functions;
+
+#endif

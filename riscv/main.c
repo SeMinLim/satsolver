56b5b1ca2a1cfe37c4d6ce87d58765dd073d7980
SeMinLim <seminl1@ics.uci.edu>
2023-04-22 13:08:54 -0700

    modified linker script and sp location

--- riscv/main.c
+++ riscv/main.c
@@ -1,7 +1,7 @@
 #include "solver.h"
 
 
-char volatile * const printchar = (char*)0x1fffffff;
+//char volatile * const printchar = (char*)0x1fffffff;
 
 
 // Main
@@ -11,26 +11,26 @@ int main() {
         int res = solver_parse(&s);
 
         if ( res == 20 ) {
-		(*printchar) = 0x55; // U
-		(*printchar) = 0x4e; // N
-		(*printchar) = 0x53; // S
-		(*printchar) = 0x41; // A
-		(*printchar) = 0x54; // T
-		(*printchar) = 0xa; // \n
+		//(*printchar) = 0x55; // U
+		//(*printchar) = 0x4e; // N
+		//(*printchar) = 0x53; // S
+		//(*printchar) = 0x41; // A
+		//(*printchar) = 0x54; // T
+		//(*printchar) = 0xa; // \n
 	} else {
 		res = solver_solve(&s);
                 if ( res == 10 ) {
-			(*printchar) = 0x53; // S
-			(*printchar) = 0x41; // A
-			(*printchar) = 0x54; // T
-			(*printchar) = 0xa; // \n
+			//(*printchar) = 0x53; // S
+			//(*printchar) = 0x41; // A
+			//(*printchar) = 0x54; // T
+			//(*printchar) = 0xa; // \n
                 } else if ( res == 20 ) {
-			(*printchar) = 0x55; // U
-			(*printchar) = 0x4e; // N
-			(*printchar) = 0x53; // S
-			(*printchar) = 0x41; // A
-			(*printchar) = 0x54; // T
-			(*printchar) = 0xa; // \n
+			//(*printchar) = 0x55; // U
+			//(*printchar) = 0x4e; // N
+			//(*printchar) = 0x53; // S
+			//(*printchar) = 0x41; // A
+			//(*printchar) = 0x54; // T
+			//(*printchar) = 0xa; // \n
 		}
         }
 }
